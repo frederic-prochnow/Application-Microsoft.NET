69047c6704f929a74c1b559dd6304e1048eb777a
Frederic Prochnow <frederic.prochnow@numericable.fr>
2016-03-08 16:49:08 +0100

    COMMIT-08-03-2015

---
+++ Bureau/PROJET IUT - TREASURE HUNT/src/projet/Map.java
@@ -0,0 +1,11 @@
+package projet;
+
+public class Map {
+
+	public static void main(String [] args) {
+		Ile plateau = new Ile();
+		plateau.initialiser();
+		System.out.println(plateau.toString());
+	}
+
+}
