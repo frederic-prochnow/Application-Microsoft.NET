ed23a7d91282fa3f8bd13dfd58bc026383808f29
Frederic PROCHNOW <frederic.prochnow@etudiant.univ-lille1.fr>
2017-03-20 10:51:42 +0100

    2003171051

---
+++ tp7/MaSolution7/Service/Auteur.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace Service
+{
+    [DataContract]
+    public class Auteur
+    {
+        [DataMember]
+        public string NomArtiste { get; set; }
+        [DataMember]
+        public string Nom { get; set; }
+        [DataMember]
+        public string Prenom { get; set; }
+        [DataMember]
+        public List<Multimedia> Multimedia { get; set; }
+
+        public Auteur(string NomArtiste, string Nom, string Prenom)
+        {
+            this.NomArtiste = NomArtiste;
+            this.Nom = Nom;
+            this.Prenom = Prenom;
+        }
+    }
+}
---
+++ tp7/MaSolution7/Service/Multimedia.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Bo;
+using System.Runtime.Serialization;
+
+namespace Service
+{
+    [DataContract]
+    public class Multimedia
+    {
+        [DataMember]
+        public long AuteurId { get; set; }
+        [DataMember]
+        public string Titre { get; set; }
+        [DataMember]
+        public MultimediaType Type { get; set; }
+
+        public Multimedia(long AuteurId, string Titre, MultimediaType Type)
+        {
+            this.AuteurId = AuteurId;
+            this.Titre = Titre;
+            this.Type = Type;
+        }
+    }
+}
